76077aadf50905f4f86c8442cc9fcad1eb41a155
Mikhail <Mikhail.Kapusta@jetbrains.com>
2015-02-03 13:45:20 +0300

    #tp-2 Fixed Critical

--- testCCC.py
+++ testCCC.py
@@ -5,52 +5,5 @@ asdasdasdasdas
 sdfdfsfdsd
 dsdsfdsdsfdfs
 
-ssdfsdf
-sfd
-dsewr werwerwe
-we r
-asasddasdasasd
-ertertert
-ert
-ert
-er
-ertdsfdsfdsf
-ds
-fds
-sdf
-dsf
-d
-fssdf
-
-sadasd
-asdas
-dasd
-
-asd
-sd
-dsasd
-asd
-asdadsasddsa
-sda
-
-asd
-sada
-asd
-asd
-asdsadsaasdasdasd
-
-
-
-as
-sa
-a
-asd
-as
-asd
-asd
-sd
-sad
-sda
-
-fsdfsdfsdf
+sasdsasdads
  
